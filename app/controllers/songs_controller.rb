3df7f4843fa43cad20976876162334a052994de4
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2021-03-27 15:21:54 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -6,6 +6,10 @@ class SongsController < ApplicationController
         erb :'/songs/index'
     end
 
+    get "/songs/new" do
+        erb :"/songs/new"
+    end
+
     get "/songs/:slug" do 
     
         @song = Song.find_by_slug(params[:slug])
@@ -14,5 +18,16 @@ class SongsController < ApplicationController
         @genre = Genre.all.find_by(id: @song_genre.genre_id)
         #binding.pry
         erb :"/songs/show"
+    end
+
+    get "/songs/new" do
+        erb :"/songs/new"
+    end
+
+    post "/songs" do
+        #binding.pry
+        @song = Song.create(params[:song])
+        @artist = Artist.create(params[:artist])
+        redirect to "/song/#{@song.slug}"
     end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,7 @@
 # Add seed data here. Seed your database with `rake db:seed`
 
 Song.create(name: "Ostani djubre do kraja", artist_id: 1)
-    
+
+Artist.create(name: "Riblja Corba")
+
+Genre.create(name: "Rock")
